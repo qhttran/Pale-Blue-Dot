4c3051be88278c59dd0e8d50ee6545989a02f425
KasinSparks <33698320+KasinSparks@users.noreply.github.com>
2017-11-29 11:02:11 -0600

    random update

--- src/newtonlawofgravity/NewtonLawOfGravity.java
+++ src/newtonlawofgravity/NewtonLawOfGravity.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package newtonlawofgravity;
-
+// y
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
